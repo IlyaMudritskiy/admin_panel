c7590a9b3c580a350b7890d5d9095d7bf157a96b
IlyaMudritskiy <ilya.mudritskiy@gmail.com>
2023-01-12 17:58:02 +0000

    Created admin application for dovies database view and edit

---
+++ movies/admin.py
@@ -0,0 +1,44 @@
+from django.contrib import admin
+
+from .models import Genre, Filmwork, GenreFilmwork, Person, PersonFilmwork
+from .custom_filters import CreationDateListFilter, RatingListFilter, TypeListFilter
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name',)
+
+
+class GenreFilmworkInline(admin.TabularInline):
+    model = GenreFilmwork
+    fk_name = 'film_work'
+
+
+class PersonFilmworkInline(admin.TabularInline):
+    model = PersonFilmwork
+    autocomplete_fields = ('person',)
+    fk_name = 'film_work'
+
+
+@admin.register(Filmwork)
+class FilmworkAdmin(admin.ModelAdmin):
+    inlines = (GenreFilmworkInline, PersonFilmworkInline)
+    list_display = (
+        'title',
+        'type',
+        'creation_date',
+        'rating',
+        'get_genres',
+        'get_persons',
+    )
+    list_filter = (TypeListFilter, RatingListFilter, CreationDateListFilter)
+    search_fields = ('title', )
+    list_per_page = 25
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('full_name',)
+    search_fields = ('full_name', )
+    list_per_page = 25
